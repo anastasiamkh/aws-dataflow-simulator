2bef60d5328161450bc9271faa40e9d5477ea7cf
nastjamakh <nastja.makhonina.dev@gmail.com>
2024-08-19 02:10:00 +0200

    add aws_dataflow_simulator/

---
+++ aws_dataflow_simulator/streaming.py
@@ -0,0 +1,79 @@
+import boto3
+import csv
+import time
+import os
+
+import logging
+
+import src.config as config
+from src.utils_s3 import download_file_from_s3
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class CSVtoStream:
+    def __init__(self):
+        """Read the environmental variables."""
+        self.bucket_name: str = config.get_s3_bucket_name()
+        self.dataset_filepath: str = config.get_dataset_filepath(processed=True)
+        self.kinesis_stream_name: str = config.get_kinesis_stream_name()
+
+        # clients to connect to AWS services
+        self._kinesis_client = boto3.client("kinesis")
+
+    def load_dataset(self):
+        decoded_csv_data = download_file_from_s3(
+            bucket_name=self.bucket_name, filepath=self.dataset_filepath
+        )
+
+        # parse the file
+        file_content = decoded_csv_data.splitlines()
+
+        return file_content
+
+    def start_stream(self) -> None:
+        """Convert rows in csv file on AWS S3 to events in AWS Kinesis stream."""
+
+        csv_reader = csv.reader(self.load_dataset())
+
+        logger.info(
+            {
+                "message": "Starting Kinesis stream",
+                "dataset_filepath": self.dataset_filepath,
+                "s3_bucket": self.bucket_name,
+                "kinesis_stream_name": self.kinesis_stream_name,
+            }
+        )
+        # Process each row in the CSV file and send it to the Kinesis stream
+        for row in csv_reader:
+            data = ",".join(row)
+            logging.info(f"Creating event form dataset row: {data}")
+            self._kinesis_client.put_record(
+                StreamName=self.kinesis_stream_name,
+                Data=data,
+                PartitionKey=row[
+                    0
+                ],  # Assuming the first column can be used as a partition key
+            )
+
+            if "time_till_next_event_ms" in row:
+                delay_ms = int(row["time_till_next_event_ms"])
+                logging.info(f"Applying delay of {delay_ms} ms before next event.")
+                time.sleep(delay_ms / 1000.0)
+
+        logger.info(
+            {
+                "message": "Streaming to Kinesis complete. No more rows to stream",
+                "dataset_filepath": self.dataset_filepath,
+                "s3_bucket": self.bucket_name,
+                "kinesis_stream_name": self.kinesis_stream_name,
+            }
+        )
+
+        return {"statusCode": 200, "body": "Finished streaming data."}
+
+
+if __name__ == "__main__":
+    CSVtoStream().start_stream(simulate_time_delay=True)
